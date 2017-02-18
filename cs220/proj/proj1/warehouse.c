75a1062663d6ccb94802956cb9554004493e2638
Evan M. Baule <evanbaule@gmail.com>
2017-02-17 19:01:56 -0500

    Made some changes, trying to get prices as low as possible before handing in.

--- cs220/proj/proj1/warehouse.c
+++ cs220/proj/proj1/warehouse.c
@@ -8,8 +8,17 @@
 int lowestFreq();
 int frequency[ARRLEN];
 
+/*
+main() Process
+	1. Sets each value in the frequency array to 0
+	2. Declares index/working values that are used within the loop
+	3. 
+*/
 int main(int argc, char ** argv) {
 
+	/*
+	The following loop initializes all values of freq[] to 0
+	*/
 	int ze;
 	for(ze = 0; ze < ARRLEN; ze++){
 		frequency[ze] = 0;
@@ -19,10 +28,31 @@ int main(int argc, char ** argv) {
 	int lowest;
 	int idx = 0;
 	initSlots();
-
 	while(1==scanf("%d",&bin)) {
 		frequency[bin]++;
 		if (-1==findSlot(bin)) {
+			getBin(bin, idx);
+			if(idx < (NUMSLOTS - 1)){idx++;}
+			else{
+				int bindex;
+				int lowestBin = 0;
+				for(bindex = 0; bindex < 4; bindex++){
+					if(frequency[bindex] < frequency[lowestBin]){
+						lowestBin = bindex;
+					}
+				}
+				getBin(bin, lowestBin);
+			}
+		}
+		getWidget(bin);
+	}
+	printEarnings();
+	return 0;
+
+	/*
+	while(1==scanf("%d",&bin)) {
+		frequency[bin]++;
+		if (findSlot(bin) == (-1)) {
 			getBin(bin,idx);
 			if(idx < (NUMSLOTS-1)){
 				idx++;
@@ -40,33 +70,29 @@ int main(int argc, char ** argv) {
 				}
 			}
 		}
-		
-		/*if(findSlot(bin) == -1){
-			getBin(bin,idx);
-			if(idx < 3){
-				idx++;
-		}else {
-				l = lowestFreq();
+		frequency[bin]++;
+		if(findSlot(bin)== -1){
+			if(idx < (NUMSLOTS-1)){getBin(bin, idx); idx++;} 
+			else{
+				lowest = lowestFreq();
 				int j;
 				for(j = 0; j < NUMSLOTS; j++){
-					if(findSlot(l) != -1){
-						getBin(bin, j);
+					if(findSlot(lowest) != -1){
+						getBin(bin, lowest);
 					}
 					else{
-						l = l-1;
-						lowestFreq(l);
+						lowest = lowest-1;
 					}
 				}
 			}
 		}
-		*/
-
-		
 		getWidget(bin);
 	}
 	printEarnings();
 	printf("%d", lowestFreq());
 	return 0;
+	*/
+
 }
 
 int lowestFreq(){
