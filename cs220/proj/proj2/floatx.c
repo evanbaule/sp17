2f53ab685f3d3912f76eba4122142ebdc8588ece
Evan M. Baule <evanbaule@gmail.com>
2017-03-14 20:53:08 -0400

    This is going nowhere

--- cs220/proj/proj2/floatx.c
+++ cs220/proj/proj2/floatx.c
@@ -6,13 +6,20 @@
 -------------------------------------------------------------------------------- */
 floatx doubleToFloatx(const floatxDef *def, double value) {
 	/* Put your code here */
-	return 0;
+	floatx bits = *(floatx*) &value;
+	int fraction = def->totBits-def->expBits - 1;
+	
+	int exponent = 
+
+	return ret;
+
 }
 
 /** Return C double with value which best approximates that of floatx fx
  *  (as defined by *def).
  */
 double floatxToDouble(const floatxDef *def, floatx fx) {
+
 	/* Put your code here */
 	return NAN;
 }
